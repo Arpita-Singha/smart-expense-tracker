5faa3332ecc3608dfb91bd73027929f33f2cfcdb
Arpita Singha <arpita.singha@hotmail.com>
2026-07-26 23:24:45 +0530

    Fix API routes

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,16 +1,16 @@
 import api from "./api";
 
 export const registerUser = async (data) => {
-    const response = await api.post("/users/register", data);
+    const response = await api.post("/api/users/register", data);
     return response.data;
 };
 
 export const loginUser = async (data) => {
-    const response = await api.post("/users/login", data);
+    const response = await api.post("/api/users/login", data);
     return response.data;
 };
 
 export const getProfile = async () => {
-    const response = await api.get("/users/profile");
+    const response = await api.get("/api/users/profile");
     return response.data;
 };
